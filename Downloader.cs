165619845dcf93f613d41388ce13e27be8925082
李华祥 <lihuaxiang0321@msn.cn>
2020-03-28 13:27:03 +0800

    添加项目文件。

---
+++ Downloader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+namespace EAOriginUpdater
+{
+   /// <summary>
+   /// 表示一个可以下载文件的下载器类。
+   /// </summary>
+   [Serializable]
+   [ComVisible(true)]
+   public class Downloader
+   {
+      private string _sourceUri;//文件在互联网上的URL字符串。
+      private string _savedPath;//需要保存的文件路径。
+      /// <summary>
+      /// 构造函数，创建一个包含文件下载源和保存路径的Downloader实例。
+      /// </summary>
+      /// <param name="sourceUri">文件在互联网上的URL字符串，即文件下载源。</param>
+      /// <param name="savedPath">下载的文件的保存路径。</param>
+      public Downloader(string sourceUri, string savedPath)
+      {
+         SourceUri = sourceUri;
+         SavedPath = savedPath;
+      }
+      /// <summary>
+      /// 获取或设置当前实例的文件下载源。
+      /// </summary>
+      public string SourceUri { get => _sourceUri; set => _sourceUri = value; }
+      /// <summary>
+      /// 获取或设置当前实例的文件保存路径。
+      /// </summary>
+      public string SavedPath { get => _savedPath; set => _savedPath = value; }
+      /// <summary>
+      /// 通过HTTP的方式下载指定的文件，其下载源和保存路径由SourceUri属性和SavedPath属性指定。
+      /// </summary>
+      public void HttpDownload()
+      {
+         HttpWebRequest request = WebRequest.Create(SourceUri) as HttpWebRequest;
+         HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+         Stream responseStream = response.GetResponseStream();
+         Stream stream = new FileStream(SavedPath, FileMode.Create);
+         byte[] bArr = new byte[1024];
+         int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+         while (size > 0)
+         {
+            stream.Write(bArr, 0, size);
+            size = responseStream.Read(bArr, 0, (int)bArr.Length);
+         }
+         stream.Close();
+         responseStream.Close();
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+namespace EAOriginUpdater
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         Uri downlaodUri = GetDownloadUri();
+         if (downlaodUri == null) Console.WriteLine("INFO : EA Origin do not update or not found download source!");
+         else
+         {
+            Console.WriteLine("INFO : Would you like to download it here or use a third-party tool?");
+            Console.WriteLine("[ OPTION ] now : Download now within the application.");
+            Console.WriteLine("[ OPTION ] later : Download later using a third-party application.");
+            string options = Console.ReadLine();
+            switch (options.ToUpper())
+            {
+               case "NOW":
+                  Console.WriteLine("INFO : Downloading...");
+                  _ = new Downloader(GetDownloadUri().ToString(), SavedPath);
+                  bool isComplete = File.Exists(SavedPath);
+                  Console.WriteLine($"INFO : Download {(isComplete ? "completed" : "failed")}...");
+                  break;
+               case "LATER":
+                  SetText(downlaodUri.ToString());
+                  Console.WriteLine("INFO : The download address of EA Origin update package has been copied to the Windows Clipboard!");
+                  break;
+               default:
+                  Console.WriteLine("WARNNING : Please enter the correct options to perform the operation of the application!");
+                  break;
+            }
+         }
+         Console.WriteLine(DateTime.Now.ToString());
+         Console.Write("\nPress any key to exit...");
+         Console.ReadKey(false);
+      }
+      [DllImport("User32")]
+      internal static extern bool OpenClipboard(IntPtr hWndNewOwner);
+      [DllImport("User32")]
+      internal static extern bool CloseClipboard();
+      [DllImport("User32")]
+      internal static extern bool EmptyClipboard();
+      [DllImport("User32", CharSet = CharSet.Unicode)]
+      internal static extern IntPtr SetClipboardData(int uFormat, IntPtr hMem);
+      public static string SavedPath { get; private set; }
+      public static void SetText(string text)
+      {
+         if (!OpenClipboard(IntPtr.Zero))
+         {
+            SetText(text);
+            return;
+         }
+         EmptyClipboard();
+         SetClipboardData(13, Marshal.StringToHGlobalUni(text));
+         CloseClipboard();
+      }
+      public static Uri GetDownloadUri()
+      {
+         const string downlaodUriSubString = @"https://download.dm.origin.com/origin/live/";
+         string systemDisk = Environment.SystemDirectory.Substring(0, 1);
+         string originSelfUpdateDirectory = $@"{systemDisk}:\ProgramData\Origin\SelfUpdate";
+         string originSelfUpdateDirectoryWithAllUsers = $@"{systemDisk}:\Documents and Settings\All Users\Origin\SelfUpdate";
+         string[] filesCurr = Directory.GetFiles(originSelfUpdateDirectory);
+         string[] filesAll = Directory.GetFiles(originSelfUpdateDirectoryWithAllUsers);
+         string downlaodUri = downlaodUriSubString;
+         bool isFoundedFile = false;
+         foreach (string file in filesCurr)
+         {
+            if (new FileInfo(file).Extension == ".part")
+            {
+               downlaodUri += Path.GetFileNameWithoutExtension(file);
+               isFoundedFile = true;
+               SavedPath = $@"{originSelfUpdateDirectory}\{Path.GetFileNameWithoutExtension(file)}";
+               break;
+            }
+         }
+         if (!isFoundedFile)
+         {
+            foreach (string file in filesAll)
+            {
+               if (new FileInfo(file).Extension == ".part")
+               {
+                  downlaodUri += Path.GetFileNameWithoutExtension(file);
+                  isFoundedFile = true;
+                  SavedPath = $@"{originSelfUpdateDirectoryWithAllUsers}\{Path.GetFileNameWithoutExtension(file)}";
+                  break;
+               }
+            }
+         }
+         if (isFoundedFile) return new Uri(downlaodUri);
+         else return null;
+      }
+   }
+}
